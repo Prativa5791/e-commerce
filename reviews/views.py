978f7cbb2778ff67883b3c51d224703d5fe0021b
Umesh <ums.rbc07@gmail.com>
2026-01-15 12:14:45 +0545

    views for reviews

--- reviews/views.py
+++ reviews/views.py
@@ -1,6 +1,14 @@
 from rest_framework.permissions import IsAuthenticated
-from rest_framework.decorators import action
-from rest_framework.response import Response
-from django.db.models import Sum
+from rest_framework import viewsets
+from .serializers import ReviewSerializer
+from .models import Review
 
+class ReviewViewSet(viewsets.ModelViewSet):
+    serializer_class = ReviewSerializer
+    permission_classes = [IsAuthenticated]
 
+    def get_queryset(self):
+        return Review.objects.filter(user=self.request.user)
+    
+    def perform_create(self,serializer):
+        serializer.save(user=self.request.user)
